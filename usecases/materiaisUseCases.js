46334b60c0fcd7f9243f00320405adbf36b3a5ad
Gabriel Anael Strotkamp <20221PF.CC0008@passofundo.ifsul.local>
2025-12-05 10:34:23 -0300

    atualizacao dia 05-12

--- usecases/materiaisUseCases.js
+++ usecases/materiaisUseCases.js
@@ -9,7 +9,51 @@ const getMateriaisDB = async () => {
         throw "Erro : " + err;
     }
 }
+const addMateriaisDB = async (body) => {
+    try {   
+        const { nome } = body; 
+        const results = await pool.query(`INSERT INTO Materiais (descricao) 
+            VALUES ($1)
+            returning materialID, descricao, valor`,
+        [nome]);
+        const materiais = results.rows[0];
+        return new materiais(materiais.materialID, materiais.descricao, materiais.valor); 
+    } catch (err) {
+        throw "Erro ao inserir o cliente: " + err;
+    }    
+}
+
+
+const updateMateriasDB = async (body) => {
+    try {   
+        const {descricao, valor}  = body; 
+        const results = await pool.query(`UPDATE Materias set valor = $3 where descricao = $2 where materialID = $1
+        returning, materialID, descricao, valor`,
+        [materialID, descricao, valor]);        
+        if (results.rowCount == 0){
+            throw `Nenhum registro encontrado com o id ${materialID} para ser alterado`;
+        }
+        const Materiais = results.rows[0];
+        return new Materiais(Materiais.materialID, Materiais.descricao, materiais.valor); 
+    } catch (err) {
+        throw "Erro ao alterar o Material: " + err;
+    }      
+}
+
+const deleteMateriaisDB = async (materialID) => {
+    try {           
+        const results = await pool.query(`DELETE FROM materiais where materialID = $1`,
+        [materialID]);
+        if (results.rowCount == 0){
+            throw `Nenhum registro encontrado com o id ${materialID} para ser removido`;
+        } else {
+            return "material removida com sucesso";
+        }       
+    } catch (err) {
+        throw "Erro ao remover o Mateiral: " + err;
+    }     
+}
 
 module.exports = {
-    getMateriaisDB
+    getMateriaisDB, addMateriaisDB, updateMateriasDB, deleteMateriaisDB
 }
--- usecases/pedidosUseCases.js
+++ usecases/pedidosUseCases.js
@@ -11,7 +11,50 @@ const getPedidosDB = async () => {
         throw "Erro : " + err;
     }
 }
+const addPedidosDB = async (body) => {
+    try {   
+        const { nome } = body; 
+        const results = await pool.query(`INSERT INTO clientes (nome) 
+            VALUES ($1)
+            returning clienteID, nome`,
+        [nome]);
+        const Clientes = results.rows[0];
+        return new Clientes(cliente.clienteID, cliente.nome); 
+    } catch (err) {
+        throw "Erro ao inserir o cliente: " + err;
+    }    
+}
 
+
+const updatePedidosDB = async (body) => {
+    try {   
+        const { clienteID, nome }  = body; 
+        const results = await pool.query(`UPDATE Clientes set nome = $2 where clienteID = $1 
+        returning clienteID, nome`,
+        [clienteID, nome]);        
+        if (results.rowCount == 0){
+            throw `Nenhum registro encontrado com o id ${clienteID} para ser alterado`;
+        }
+        const Clientes = results.rows[0];
+        return new Clientes(clientes.clienteID, clientes.nome); 
+    } catch (err) {
+        throw "Erro ao alterar o cliente: " + err;
+    }      
+}
+
+const deletePedidosDB = async (clienteID) => {
+    try {           
+        const results = await pool.query(`DELETE FROM clientes where clienteID = $1`,
+        [clienteID]);
+        if (results.rowCount == 0){
+            throw `Nenhum registro encontrado com o id ${clienteID} para ser removido`;
+        } else {
+            return "cliente removida com sucesso";
+        }       
+    } catch (err) {
+        throw "Erro ao remover o cliente: " + err;
+    }     
+}
 module.exports = {
     getPedidosDB
 }
